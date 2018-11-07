fe52fa3dab56623c5c5cd53ee97763525f54d445
Matt Cockayne <matthew.cockayne@md.catapult.org.uk>
2018-11-07 11:09:26 +0000

    corrects testing for lint and test

--- klein_config/test_config.py
+++ klein_config/test_config.py
@@ -1,9 +1,8 @@
-from test.support import EnvironmentVarGuard
+import mock
 import pytest
+import os
 from .config import EnvironmentAwareConfig
 
-env = EnvironmentVarGuard()
-env.set('ENV_KEY', 'env_value')
 initial_config = dict()
 initial_config["key"] = "value"
 initial_config["level1"] = dict()
@@ -11,7 +10,7 @@ initial_config["level1"]["level2"] = dict()
 initial_config["level1"]["level2"]["key"] = "value"
 config = EnvironmentAwareConfig(initial_config)
 
-
+@mock.patch.dict(os.environ,{'ENV_KEY':'env_value'})
 def test_for_valid_environment_key_with_dot_notation():
     assert config.get("env.key") == "env_value"
 
