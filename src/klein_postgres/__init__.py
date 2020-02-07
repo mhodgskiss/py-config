1e58411ffa1740ba3b1b06a5894b3838b4d5e1af
simon.skinner <simon.skinner@md.catapult.org.uk>
2020-02-07 12:54:23 +0000

    abstact py postgres into a connection class and replace function calls with imports in postgres.py

--- src/klein_postgres/__init__.py
+++ src/klein_postgres/__init__.py
@@ -2,6 +2,6 @@ from .postgres import connection
 from .postgres import cursor
 from .postgres import connect
 from .postgres import refresh
-from .postgres import params
+from .connect import params
 
 
--- src/klein_postgres/postgres.py
+++ src/klein_postgres/postgres.py
@@ -1,84 +1,10 @@
 # -*- coding: utf-8 -*-
-import argparse
-import logging
-import psycopg2
 import psycopg2.extras
-from psycopg2.extras import LoggingConnection
 
-from klein_config import config
+from .connect import PostgresConnection
 
-parser = argparse.ArgumentParser()
-parser.add_argument("--debug", help="enable debug", action="store_true")
-
-def is_debug():
-    ARGS, _ = parser.parse_known_args()
-    return ARGS.debug
-
-
-def params(**kwargs):
-    '''
-    generate a dict of connection based on those provided by klein_config
-    :param kwargs: expanded keyword arguments to build a connection with
-    :return dict 
-    '''
-    p = dict()
-    if config.has('postgres.username'):
-        p["user"] = config.get('postgres.username')
-
-    if config.has('postgres.password'):
-        p["password"] = config.get('postgres.password')
-
-    if config.has('postgres.database'):
-        p["database"] = config.get('postgres.database')
-
-    if config.has('postgres.host'):
-        p["host"] = config.get('postgres.host', "127.0.0.1")
-
-    if config.has('postgres.port'):
-        p["port"] = config.get('postgres.port', "5432")
-
-    for key, value in kwargs.items():
-        p[key] = value
-
-    return p
-
-def refresh(**kwargs):
-    '''
-    refresh the connection
-    :param config: dict of parameters to refresh the connection with (optional)
-    :return psycopg.connection
-    '''
-    if connection: 
-        connection.close()
-    return connect(**kwargs)
-
-def connect(**kwargs):
-    '''
-    connect to database
-    :param config: dict of parameters to refresh the connection with (optional)
-    :return psycopg.connection
-    '''
-    if not kwargs:
-        kwargs = {}
-
-    conn = None
-    p = params(**kwargs)
-
-    if not p:
-        return None
-    
-    if is_debug():
-        logging.basicConfig(level=logging.DEBUG)
-        logger = logging.getLogger(__name__)
-        p["connection_factory"] = LoggingConnection
-        conn = psycopg2.connect(**p)
-        conn.initialize(logger)
-    else:
-        conn = psycopg2.connect(**p) 
-
-    return conn
-    
-connection_params = params()
-connection = connect()
-
+connection_object = PostgresConnection()
+connect = connection_object.connect
+connection = connection_object.connect()
+refresh = connection_object.refresh()
 cursor = connection.cursor(cursor_factory=psycopg2.extras.DictCursor) if connection else None
