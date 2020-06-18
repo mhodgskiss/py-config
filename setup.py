b947bb4c36ab8b758cebe37ae460cbc5d26e5088
Mark Laing <mark.laing@md.catapult.org.uk>
2020-06-18 16:06:58 +0100

    Increment version.

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def readme():
 
 
 setup(name='klein_config',
-      version='2.0.2',
+      version='2.0.3',
       description='Configuration detection from the command line',
       url='http://gitlab.mdcatapult.io/informatics/klein/klein-config',
       author='Matt Cockayne',
